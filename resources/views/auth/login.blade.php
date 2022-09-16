63a6b2a2bcbae055119f3056e23db7022e6dc02e
laevad <djaldave1@gmail.com>
2022-09-16 18:08:24 +0800

    fixed the login layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,13 +4,13 @@
             <div class="offset-md-2 col-lg-5 col-md-7 offset-lg-4 offset-md-3">
                 <div class="panel border bg-white p-2">
                     <div class="panel-heading">
-                        <h3 class="pt-3 font-weight-bold">{{ config('app.name', 'WT&P Management System') }}</h3>
+                        <h3 class="pt-3 font-weight-bold text-center">{{ config('app.name', 'WT&P Management System') }}</h3>
                     </div>
                     <div class="panel-body p-3">
                         <form  method="POST" action="{{ route('login') }}">
                             @csrf
                             <div>
-                                <label for="email" class="col-md-4 col-form-label text-md-end ">{{ __('Email Address') }}</label>
+                                <label for="email" class="col-md-5 col-form-label text-md-end ">{{ __('Email Address') }}</label>
                                 <input id="email" style="width: 100%" type="email" class="form-control  border @error('email') is-invalid @enderror" placeholder="Enter your Email" name="email" value="{{ old('email') }}"  autocomplete="email" autofocus>
 
                                 @error('email')
