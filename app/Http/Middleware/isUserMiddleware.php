5de9dff993861b8cfa2b520864c7715454053341
laevad <djaldave1@gmail.com>
2022-09-16 09:08:29 +0800

    init

---
+++ app/Http/Middleware/isUserMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class isUserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth('web')->check() && auth('web')->user()->role == 2){
+            return $next($request);
+        }else{
+            return redirect()->route('login');
+        }
+    }
+}
