3baf8ab7059acff90cf80947228cff68cc83f58b
laevad <djaldave1@gmail.com>
2022-09-17 07:31:46 +0800

    removed files from fortify and optimize settings for updating pass and details

--- app/Http/Livewire/Shared/Settings.php
+++ app/Http/Livewire/Shared/Settings.php
@@ -1,18 +1,22 @@
 <?php
 
 namespace App\Http\Livewire\Shared;
-use App\Actions\Fortify\UpdateUserPassword;
-use App\Actions\Fortify\UpdateUserProfileInformation;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
 class Settings extends  Component{
 
     use WithFileUploads;
-    public $image;
 
+    public $image;
     public $state;
 
 
@@ -22,6 +26,7 @@ class Settings extends  Component{
 
     }
 
+    /*UPDATE IMAGE*/
     public function updatedImage(){
         $previousPath = auth()->user()->avatar;
         $path =$this->image->store('/','avatars');
@@ -30,6 +35,7 @@ class Settings extends  Component{
         $this->dispatchBrowserEvent('updated', ['message'=>'Profile changed successfully']);
     }
 
+//    CLEAN
     protected function cleanupOldUploads()
     {
         $storage = Storage::disk('local');
@@ -40,8 +46,13 @@ class Settings extends  Component{
             }
         }
     }
-    public function updateProfile(UpdateUserProfileInformation $updateUserProfileInformation){
-        $updateUserProfileInformation->update(auth()->user(),[
+
+    /*UPDATE DETAILS*/
+    /**
+     * @throws ValidationException
+     */
+    public function updateProfile(){
+        $this->update(auth()->user(),[
             'name'=> $this->state['name'],
             'email'=> $this->state['email'],
         ]);
@@ -50,11 +61,74 @@ class Settings extends  Component{
         $this->dispatchBrowserEvent('updated', ['message'=>'Profile updated successfully']);
     }
 
-    public function changePassword(UpdateUserPassword $updateUserPassword){
-        $updateUserPassword->update(auth()->user(),Arr::only($this->state, ['current_password', 'password','password_confirmation']));
+
+    /*CHANGE PASS*/
+    /**
+     * @throws ValidationException
+     */
+    public function changePassword(){
+        $this->updatePass(auth()->user(),Arr::only($this->state, ['current_password', 'password','password_confirmation']));
         $this->state['current_password']='';
         $this->state['password']='';
         $this->state['password_confirmation']='';
         $this->dispatchBrowserEvent('updated', ['message'=>'Password updated successfully']);
     }
+
+
+
+    /*CUSTOM FUNCTION=====================================*/
+    /*custom update*/
+    /**
+     * @throws ValidationException
+     */
+    public function update($user, array $input)
+    {
+        Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique('users')->ignore($user->id),
+            ],
+        ])->validateWithBag('updateProfileInformation');
+
+        if ($input['email'] !== $user->email &&
+            $user instanceof MustVerifyEmail) {
+
+        } else {
+            $user->forceFill([
+                'name' => $input['name'],
+                'email' => $input['email'],
+            ])->save();
+        }
+    }
+
+    /*custom update pass*/
+    /**
+     * @throws ValidationException
+     */
+    public function updatePass($user, array $input)
+    {
+        Validator::make($input, [
+            'current_password' => ['required', 'string'],
+            'password' =>['required', 'string', 'confirmed',Password::min(8)
+                ->letters()
+                ->mixedCase()
+                ->numbers()
+                ->symbols()
+                ->uncompromised()],
+        ])->after(function ($validator) use ($user, $input) {
+            if (! isset($input['current_password']) || ! Hash::check($input['current_password'], $user->password)) {
+                $validator->errors()->add('current_password', __('The provided password does not match your current password.'));
+            }
+        })->validateWithBag('updatePassword');
+
+        $user->forceFill([
+            'password' => Hash::make($input['password']),
+        ])->save();
+    }
+
 }
