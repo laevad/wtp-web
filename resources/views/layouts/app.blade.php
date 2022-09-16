197c4abdabb6515654030ebbe9ae4af577fd7933
laevad <djaldave1@gmail.com>
2022-09-16 11:16:55 +0800

    success livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @livewireStyles
 </head>
 <body>
     <div id="app">
@@ -77,4 +78,5 @@
         </main>
     </div>
 </body>
+@livewireScripts
 </html>
