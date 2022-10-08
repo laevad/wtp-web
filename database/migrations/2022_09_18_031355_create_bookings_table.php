dd77d5dfd3eab90d94322089e689891001e6a74a
laevad <djaldave1@gmail.com>
2022-10-08 11:51:31 +0800

    add nullable to the migration that might be

--- database/migrations/2022_09_18_031355_create_bookings_table.php
+++ database/migrations/2022_09_18_031355_create_bookings_table.php
@@ -17,11 +17,11 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->uuid('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->uuid('vehicle_id');
+            $table->uuid('vehicle_id')->nullable();
             $table->foreign('vehicle_id')->references('id')->on('vehicles');
             $table->string('t_trip_start')->nullable();
             $table->string('t_trip_end')->nullable();
-            $table->uuid('driver_id');
+            $table->uuid('driver_id')->nullable();
             $table->foreign('driver_id')->references('id')->on('users');
             $table->string('trip_start_date')->nullable();
             $table->string('trip_end_date')->nullable();
