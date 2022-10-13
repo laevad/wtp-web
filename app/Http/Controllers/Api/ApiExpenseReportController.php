4bc0c12a035c285ce1b08123a16b232f2a0e5125
laevad <djaldave1@gmail.com>
2022-10-13 13:38:39 +0800

    added booking_id and cash_type_id

--- app/Http/Controllers/Api/ApiExpenseReportController.php
+++ app/Http/Controllers/Api/ApiExpenseReportController.php
@@ -74,6 +74,7 @@ class ApiExpenseReportController extends Controller
     public function addExpense(Request $request){
         $validators = Validator::make($request->all(), [
             'expense_type_id' => 'required|exists:expense_types,id',
+            'booking_id' => 'required|exists:bookings,id',
             'amount' => 'required|numeric',
             'description'=>'nullable'
         ]);
@@ -82,15 +83,18 @@ class ApiExpenseReportController extends Controller
             'expense_type_id' => $errors->first('expense_type_id'),
             'amount' => $errors->first('amount'),
             'description' => $errors->first('description'),
+            'booking_id' => $errors->first('booking_id'),
         ];
         if ($validators->fails()){
             return response()->json(['errors' => $err], 422);
         }
         $expense = new Cash;
         $expense->date = now()->toFormattedDate();
+        $expense->cash_type_id = 1;
         $expense->expense_type_id = $request->input('expense_type_id');
         $expense->amount = $request->input('amount');
         $expense->note = $request->input('description');
+        $expense->booking_id = $request->input('booking_id');
         $expense->save();
         return response()->json(['errors' => $err, 'data'=> $expense]);
     }
