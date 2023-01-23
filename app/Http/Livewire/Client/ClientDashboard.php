2840245f3913716765863fa6af55610a630005f4
Al Dave A <77134175+laevad@users.noreply.github.com>
2023-01-24 04:21:06 +0800

    can't login to client

--- app/Http/Livewire/Client/ClientDashboard.php
+++ app/Http/Livewire/Client/ClientDashboard.php
@@ -10,13 +10,12 @@ class ClientDashboard extends Component
     public function render()
     {
         $location = Location::join('bookings', 'bookings.driver_id', '=', 'locations.user_id')
-
-
-            ->where('bookings.user_id', '=', auth()->user()->id)
-       ->get();
+            ->where('bookings.user_id', '=', auth()->user()->id)->get();
+        $offStatus = $location->where('status_id', '=', 1)->count();
         return view('livewire.client.client-dashboard', [
-            'location'=>$location
-        ]
+                'location' => $location,
+                'offStatus' => $offStatus,
+            ]
         );
     }
 }
